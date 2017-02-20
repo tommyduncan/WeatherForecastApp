948fbd467e60a36fc734eff04531c172f03cd176
Tommy <lamborghini801021@gmail.com>
2017-02-20 18:47:24 +0800

    Add location function.

--- WeatherForecast/www/scripts/controllers/homeController.js
+++ WeatherForecast/www/scripts/controllers/homeController.js
@@ -1,6 +1,14 @@
-﻿angular.module('WeatherForecast').controller('HomeController', ['$scope', 'WeatherService', function ($scope, WeatherService) {
+﻿angular.module('WeatherForecast').controller('HomeController', ['$scope', 'WeatherService', 'GeocodingService', function ($scope, WeatherService, GeocodingService) {
     WeatherService.getWeatherForecastData(function (data) {
         console.log(data);
         $scope.weatherForecastData = data.cwbopendata.dataset.location;
     });
+
+    navigator.geolocation.getCurrentPosition(function (position) {
+        GeocodingService.getCityName(position.coords.latitude, position.coords.longitude, function (data) {
+            $scope.nowLocation = data.results[0].address_components[3].long_name;
+        });
+    }
+    , function (error) {
+    });    
 }]);
---
+++ WeatherForecast/www/scripts/services/geocodingService.js
@@ -0,0 +1,14 @@
+﻿angular.module('WeatherForecast').service('GeocodingService', function ($http) {
+    this.getCityName = function (lat, lng, onSuccess) {
+        $http.get('http://maps.google.com/maps/api/geocode/json?latlng=' + lat + ',' + lng + '&language=zh-TW&sensor=true').
+        success(function (data, status, headers, config) {
+            (onSuccess || angular.noop)(data);
+        }).
+        error(function (data, status, headers, config) {
+            console.log("Error Data:" + data);
+            console.log('Status: ' + status);
+            console.log('Headers: ' + headers);
+            console.log('Config: ' + config);
+        });
+    };
+});
