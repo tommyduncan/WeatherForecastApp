4bccd07f15d4b27a16b6013ab7042da975a60390
Tommy <lamborghini801021@hotmail.com>
2017-02-22 00:41:32 +0800

    Only show the weather information of current location.

--- WeatherForecast/www/scripts/controllers/homeController.js
+++ WeatherForecast/www/scripts/controllers/homeController.js
@@ -1,14 +1,32 @@
-﻿angular.module('WeatherForecast').controller('HomeController', ['$scope', 'WeatherService', 'GeocodingService', function ($scope, WeatherService, GeocodingService) {
-    WeatherService.getWeatherForecastData(function (data) {
-        console.log(data);
-        $scope.weatherForecastData = data.cwbopendata.dataset.location;
+﻿angular.module('WeatherForecast').controller('HomeController', ['$scope', '$ionicLoading', 'WeatherService', 'GeocodingService', function ($scope, $ionicLoading, WeatherService, GeocodingService) {
+    
+    $ionicLoading.show({
+      template: "<ion-spinner icon='ios'></ion-spinner>",
+      duration: 2000
+    }).then(function(){
+       
     });
 
     navigator.geolocation.getCurrentPosition(function (position) {
         GeocodingService.getCityName(position.coords.latitude, position.coords.longitude, function (data) {
-            $scope.nowLocation = data.results[0].address_components[3].long_name;
+            if(data.results[0].address_components[4].long_name === '台北市')
+                currentCity = '臺北市';
+            else
+                currentCity = data.results[0].address_components[4].long_name;
+
+            $scope.nowLocation = currentCity;
+
+            WeatherService.getWeatherForecastData(function (data) {
+                for(key in data.cwbopendata.dataset.location){
+                    if(data.cwbopendata.dataset.location[key].locationName === currentCity){
+                        $scope.weatherData = data.cwbopendata.dataset.location[key];
+                        console.log(data.cwbopendata.dataset.location[key].weatherElement);
+                    }
+                }
+            });
         });
     }
     , function (error) {
-    });    
+        console.log(error);
+    });
 }]);
